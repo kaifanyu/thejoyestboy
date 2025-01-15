0c560e11643e5ddcadbd31d792c737ff67409584
kaifanyu <kaifany1@uci.edu>
2025-01-15 15:56:22 -0800

    l

--- src/Steps/Phone.jsx
+++ src/Steps/Phone.jsx
@@ -8,7 +8,7 @@ function Phone({ phoneNumber, setPhoneNumber, handleSubmit }) {
 
   const handleSkip = () => {
     setPhoneNumber('');
-    nextStep();
+    handleSubmit('');
   };
 
   const handleHelp = () => {
